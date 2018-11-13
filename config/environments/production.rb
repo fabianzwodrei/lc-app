4bc7dba283c6c2cff8b3f8398b0c403a195c5a78
fabianzwodrei <fabian@zwodrei.com>
2018-11-13 16:57:29 +0100

    fixes wrong host url in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ENV['MAIL_ADDR'] }
+  config.action_mailer.default_url_options = { host: ENV['ROOT_URL'] }
   config.action_mailer.raise_delivery_errors = true 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
